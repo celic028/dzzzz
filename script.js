67c3676ecf2e7e5cf839fbbd140fd7ce78b73093
celic028 <celicgogleucionica@gmail.com>
2025-02-02 19:29:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.getElementById('yesBtn').addEventListener('click', function() {
 });
 
 document.getElementById('noBtn').addEventListener('click', function() {
-    document.getElementById('responseMessage').innerHTML = 'ok briga me mrzim prasa mo ne obracaj mi se ';
+    document.getElementById('responseMessage').innerHTML = 'ok briga me mrzim praska mo ne obracaj mi se mrs';
     document.getElementById('responseMessage').style.color = '#f44336'; // Red for sympathy
 });
